2c2cd7c7c2b4b5681bf906438b19970474b74b5a
aidagalimova <aidagalimova03@gmail.com>
2021-08-19 23:31:25 +0300

    V-01 fix phone number link

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,7 +6,9 @@ function MainPageFooter() {
     <div className="footer-div">
       <h3 className="text regular">© 2016-2019 «Need for drive»</h3>
       <div className="number-div">
-        <h3 className="number regular">8 (495) 234-22-44</h3>
+        <a href="tel: +74952342244" className="number regular">
+          8 (495) 234-22-44
+        </a>
       </div>
     </div>
   );
