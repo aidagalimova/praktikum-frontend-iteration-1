ae1bcd94756963e074475cd594eef5cae16fa5bf
aidagalimova <aidagalimova03@gmail.com>
2021-08-19 23:03:07 +0300

    V-01 add slider highlight, fix arrow svg

--- src/components/slider-element/index.jsx
+++ src/components/slider-element/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Button } from "antd";
+import { Button } from "antd";
 import { ReactComponent as Arrow } from "../../assets/svg/arrow.svg";
 import "./index.scss";
 
@@ -11,26 +11,22 @@ function SliderElement({ el }) {
         backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #000000 100%), url(${el.img})`,
       }}
     >
-      <Row>
-        <Col span={1}>
-          <Arrow className="left-arrow" />
-        </Col>
-        <Col span={22}>
-          <div className="text-div">
-            <h1 className="name medium">{el.name}</h1>
-            <h3 className="discription light">{el.discription}</h3>
-            <Button className={`btn btn-${el.id}`}>
-              <h3 className="btn-text medium">Подбробнее </h3>
-            </Button>
-          </div>
-        </Col>
-        <Col span={1}>
-          <Arrow
-            className="right-arrow"
-            transform="scale(-1, 1) translate(-10, 0)"
-          />
-        </Col>
-      </Row>
+      <div className="arrow">
+        <Arrow className="arrow-icon" />
+      </div>
+      <div className="text-div">
+        <h1 className="name medium">{el.name}</h1>
+        <h3 className="discription light">{el.discription}</h3>
+        <Button className={`btn btn-${el.id}`}>
+          <h3 className="btn-text medium">Подбробнее </h3>
+        </Button>
+      </div>
+      <div className="arrow">
+        <Arrow
+          className="arrow-icon"
+          transform="scale(-1, 1)"
+        />
+      </div>
     </div>
   );
 }
