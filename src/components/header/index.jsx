9d20989a1d7197cf41947a7c4ed18eaec7ecf4be
aidagalimova <aidagalimova03@gmail.com>
2021-08-19 21:22:17 +0300

    V-01 fix footer and header, add flexbox

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,7 +5,7 @@ import "./index.scss";
 function MainPageHeader() {
   return (
     <div className="header-div">
-      <h1 className="header-name">Need for drive</h1>
+        <h1 className="name">Need for drive</h1>
       <div className="city-div">
         <MapIcon className="map-icon" />
         <h3 className="city">Ульяновск</h3>
