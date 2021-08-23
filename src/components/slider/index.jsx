1720dc6ca33ef51617c389ae99c5c0415b6dcbe3
aidagalimova <aidagalimova03@gmail.com>
2021-08-23 18:53:27 +0300

    F-01 put const in file

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -3,39 +3,8 @@ import { Carousel } from "antd";
 import SliderElement from "../slider-element";
 import { ReactComponent as Dot } from "../../assets/svg/dot.svg";
 import { ReactComponent as Arrow } from "../../assets/svg/arrow.svg";
-import slider1 from "../../assets/image/sliderImg1.png";
-import slider2 from "../../assets/image/sliderImg2.png";
-import slider3 from "../../assets/image/sliderImg3.png";
-import slider4 from "../../assets/image/sliderImg4.png";
+import sliderElements from "./consts";
 import "./index.scss";
-
-const sliderElements = [
-  {
-    id: 1,
-    name: "Бесплатная парковка",
-    discription: `Оставляйте машину на платных городских парковках и разрешенных местах, 
-      не нарушая ПДД, а также в аэропортах`,
-    img: slider1,
-  },
-  {
-    id: 2,
-    name: "Страховка",
-    discription: `Полная страховка страховка автомобиля`,
-    img: slider2,
-  },
-  {
-    id: 3,
-    name: "Бензин",
-    discription: `Полный бак на любой заправке города за наш счёт`,
-    img: slider3,
-  },
-  {
-    id: 4,
-    name: "Обслуживание",
-    discription: `Автомобиль проходит еженедельное ТО`,
-    img: slider4,
-  },
-];
 
 function Slider() {
   const carousel = useRef();
