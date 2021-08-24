12c282417f97f3c2fcfafd97673da084498a7bf2
aidagalimova <aidagalimova03@gmail.com>
2021-08-24 22:15:17 +0300

    V-02 add consts file

--- src/pages/make-order-page/index.jsx
+++ src/pages/make-order-page/index.jsx
@@ -5,35 +5,8 @@ import FormSteps from "../../components/steps";
 import Sidebar from "../../components/sidebar";
 import MainPageHeader from "../../components/header";
 import OrderInfo from "../../components/order-info";
+import locations from "./consts";
 import "./index.scss";
-
-const locations = [
-  {
-    id: 0,
-    city: "Казань",
-    points: [
-      { id: 0, name: "Нариманова 42" },
-      { id: 1, name: "Нариманова 43" },
-      { id: 2, name: "Нариманова 44" },
-      { id: 3, name: "Нариманова 45" },
-    ],
-  },
-  {
-    id: 1,
-    city: "Ульяновск",
-    points: [{ id: 0, name: "Нариманова 42" }],
-  },
-  {
-    id: 2,
-    city: "Москва",
-    points: [{ id: 0, name: "Нариманова 42" }],
-  },
-  {
-    id: 3,
-    city: "Самара",
-    points: [{ id: 0, name: "Нариманова 42" }],
-  },
-];
 
 function MakeOrderPage() {
   const [order, setOrder] = useState({});
@@ -52,7 +25,7 @@ function MakeOrderPage() {
 
   return (
     <div className="page">
-      <Sidebar isMain={false}/>
+      <Sidebar isMain={false} />
       <div className="page-content">
         <Row justify="center">
           <Col flex="1310px">
@@ -62,7 +35,7 @@ function MakeOrderPage() {
             <Divider />
             <FormSteps />
             <Divider />
-            <Row >
+            <Row>
               <Col flex="0 0 905px" className="location-form-col">
                 <LocationForm
                   order={{ city: order.city, points: order.points }}
