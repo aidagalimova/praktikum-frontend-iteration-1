c7f54ce1e7da589ca66f7b3863453e96f89a4206
aidagalimova <aidagalimova03@gmail.com>
2021-08-24 22:23:20 +0300

    V-02 add price default value

--- src/components/order-info/index.jsx
+++ src/components/order-info/index.jsx
@@ -26,15 +26,13 @@ function OrderInfo({ info }) {
       </div>
       <div className="price-div">
         <div>
-          <h3 className="price-text">Цена: </h3>
+          <h3 className="price-text">Цена:&nbsp;</h3>
         </div>
-        {info.point ? (
-          <div>
-            <h3 className="price">&nbsp; от 8 000 до 12 000 ₽</h3>
-          </div>
-        ) : (
-          <></>
-        )}
+        <div>
+          <h3 className="price">
+            {info.point ? "от 8 000 до 12 000 ₽" : "0 ₽"}
+          </h3>
+        </div>
       </div>
       <Button className={`btn ${info.point ? "" : "btn-disabled"}`}>
         <h3 className="btn-text">Выбрать модель</h3>
