ca21a1bf0ec4993f954cfc393406d67496778bbf
aidagalimova <aidagalimova03@gmail.com>
2021-09-07 01:23:07 +0300

    V-06 add react-router, routing and order page

--- src/pages/routing.jsx
+++ src/pages/routing.jsx
@@ -7,12 +7,14 @@ import {
 } from "react-router-dom";
 import MakeOrderPage from "./make-order-page";
 import OrderPage from "./order-page";
+import MainPage from "./main-page";
 
 function Routing() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={MakeOrderPage} exact />
+        <Route path="/" component={MainPage} exact />
+        <Route path="/order" component={MakeOrderPage} exact />
         <Route path="/:id" component={OrderPage} exact />
         <Redirect to="/" />
       </Switch>
