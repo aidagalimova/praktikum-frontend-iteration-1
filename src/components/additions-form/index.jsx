0be406ae3c0c12025c8bdc85f8ca3efa6bd21da5
aidagalimova <aidagalimova03@gmail.com>
2021-09-11 14:55:10 +0300

    F-02 add redux hooks for additions form components

--- src/components/additions-form/index.jsx
+++ src/components/additions-form/index.jsx
@@ -1,59 +1,26 @@
 import React from "react";
-import { connect } from "react-redux";
-import {
-  colorChange,
-  tariffChange,
-  servicesChange
-} from "../../store/actions/order-info";
 import ColorRadios from "./color-radios";
 import RentalPeriodInput from "./rental-period-input";
 import TariffRadios from "./tariff-radios";
 import ServicesCheckboxes from "./services-checkboxes";
 import "./index.scss";
 
-const mapStateToProps = (state) => ({
-  color: state.order.color,
-  tariff: state.order.tariff,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  handleColorChange: (e) => {
-    dispatch(colorChange(e.target.value));
-  },
-  handleTariffChange: (e) => {
-    dispatch(tariffChange(e.target.value));
-  },
-  handleServicesChange: (value) => {
-    dispatch(servicesChange(value));
-  },
-});
-
-function AdditionsForm({
-  handleColorChange,
-  handleDurationChange,
-  handleTariffChange,
-  handleServicesChange,
-  color,
-  tariff,
-}) {
+function AdditionsForm() {
   return (
     <div className="additions-div">
       <h3 className="title light">Цвет</h3>
-      <ColorRadios handleRadioChange={handleColorChange} radioValue={color} />
+      <ColorRadios />
 
       <h3 className="title light">Дата аренды</h3>
-      <RentalPeriodInput handleDurationChange={handleDurationChange} />
+      <RentalPeriodInput />
 
       <h3 className="title light">Тариф</h3>
-      <TariffRadios
-        handleRadioChange={handleTariffChange}
-        radioValue={tariff}
-      />
+      <TariffRadios />
 
       <h3 className="title light">Доп услуги</h3>
-      <ServicesCheckboxes handleCheckboxChange={handleServicesChange} />
+      <ServicesCheckboxes />
     </div>
   );
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(AdditionsForm);
+export default AdditionsForm;
--- src/components/additions-form/services-checkboxes/index.jsx
+++ src/components/additions-form/services-checkboxes/index.jsx
@@ -1,20 +1,26 @@
 import React from "react";
-import {Checkbox} from "antd";
+import { Checkbox } from "antd";
 import "./index.scss";
+import { useDispatch } from "react-redux";
+import { servicesChange } from "../../../store/actions/order-info";
 
-function ServicesCheckboxes({ handleCheckboxChange }) {
-    const options = [
-      { label: "Полный бак, 500р", value: "Полный бак" },
-      { label: "Детское кресло, 200р", value: "Детское кресло" },
-      { label: "Правый руль, 1600р", value: "Правый руль" },
-    ];
-    return (
-      <Checkbox.Group
-        options={options}
-        onChange={handleCheckboxChange}
-        className="light"
-      />
-    );
-  }
-  
-  export default ServicesCheckboxes;
+function ServicesCheckboxes() {
+  const dispatch = useDispatch();
+  const handleServicesChange = (value) => {
+    dispatch(servicesChange(value));
+  };
+  const options = [
+    { label: "Полный бак, 500р", value: "Полный бак" },
+    { label: "Детское кресло, 200р", value: "Детское кресло" },
+    { label: "Правый руль, 1600р", value: "Правый руль" },
+  ];
+  return (
+    <Checkbox.Group
+      options={options}
+      onChange={handleServicesChange}
+      className="light"
+    />
+  );
+}
+
+export default ServicesCheckboxes;
