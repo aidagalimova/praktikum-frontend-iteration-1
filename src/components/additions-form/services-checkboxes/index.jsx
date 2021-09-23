9cc1575ac307c4950dea1bf3fc8590b9c609d8e6
aidagalimova <aidagalimova03@gmail.com>
2021-09-23 23:01:12 +0300

    F-06 change services component

--- src/components/additions-form/services-checkboxes/index.jsx
+++ src/components/additions-form/services-checkboxes/index.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import { Checkbox } from "antd";
 import "./index.scss";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { servicesChange } from "../../../store/actions/order-info";
 
 function ServicesCheckboxes() {
   const dispatch = useDispatch();
+  const services = useSelector((state) => state.order.services);
   const handleServicesChange = (value) => {
     dispatch(servicesChange(value));
   };
   const options = [
-    { label: "Полный бак, 500р", value: "Полный бак" },
-    { label: "Детское кресло, 200р", value: "Детское кресло" },
-    { label: "Правый руль, 1600р", value: "Правый руль" },
+    { label: "Полный бак, 500р", value: "Полный бак, 500р" },
+    { label: "Детское кресло, 200р", value: "Детское кресло, 200р" },
+    { label: "Правый руль, 1600р", value: "Правый руль, 1600р" },
   ];
   return (
     <Checkbox.Group
       options={options}
       onChange={handleServicesChange}
       className="light"
+      value={services}
     />
   );
 }
