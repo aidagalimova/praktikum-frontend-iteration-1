919e3a697e5ee8cd4bc5cfccabcbc3a9c66f44a2
aidagalimova <aidagalimova03@gmail.com>
2021-09-24 16:35:43 +0300

    F-06 fix datepicker

--- src/components/additions-form/rental-period-input/index.jsx
+++ src/components/additions-form/rental-period-input/index.jsx
@@ -31,7 +31,7 @@ function RentalPeriodInput() {
         duration[1] = dateDuration.asHours() % 24;
       }
     }
-    if ((duration[0] > 0 && duration[1] > 0) || dateDuration === undefined)
+    if (duration[0] > 0 || duration[1] > 0 || dateDuration === undefined)
       dispatch(dateDuartionChange(duration));
   };
 
