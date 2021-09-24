6f2ba9cdb40ae1ac421aef4d039d5e558057921c
aidagalimova <aidagalimova03@gmail.com>
2021-09-24 15:44:48 +0300

    F-06 add get locations functions

---
+++ src/store/actions/locations.js
@@ -0,0 +1,13 @@
+export const setCities = (value) => ({
+    type: "SET_CITIES",
+    payload: {
+        cities: value,
+    },
+})
+
+export const setPoints = (value) => ({
+    type: "SET_POINTS",
+    payload: {
+        points: value,
+    },
+})
---
+++ src/store/reducers/locations.js
@@ -0,0 +1,21 @@
+const initState = {
+    cities: [],
+    points: []
+}
+export default function LocationsReducer(state = initState, action) {
+    switch (action.type) {
+        case "SET_CITIES":
+            return {
+                ...state,
+                cities: action.payload.cities
+            }
+
+        case "SET_POINTS":
+            return {
+                ...state,
+                points: action.payload.points
+            }
+        default:
+            return state;
+    }
+}
